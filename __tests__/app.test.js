b2b274906a2ae6843fe134fff60554d91a9a9131
Faisal <faisalmaya@hotmail.com>
2023-03-29 11:14:41 +0100

    extra tests for POST

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -161,6 +161,19 @@ describe("/api/reviews/review_id/comments", () => {
         expect(comment.body).toBe('very nice stuff!')
         expect(comment).toHaveProperty('created_at')
       })
+    });it('should ignore extra comment properties and return a comment with status 201', () => {
+      return request(app)
+      .post('/api/reviews/3/comments')
+      .send({username: 'bainesface', body:'very nice stuff!', size: 'medium'})
+      .expect(201)
+      .then(({body})=>{
+        const {comment} = body
+        expect(comment.comment_id).toBe(7)
+        expect(comment.review_id).toBe(3)
+        expect(comment.author).toBe('bainesface')
+        expect(comment.body).toBe('very nice stuff!')
+        expect(comment).toHaveProperty('created_at')
+      })
     });
     it('should respond with a 404 status and an error message for all review IDs that don\'t exist', () => {
       return request(app)
@@ -171,7 +184,16 @@ describe("/api/reviews/review_id/comments", () => {
           expect(body.msg).toBe('Review ID does not exist')
       })
     });
-    it('should respond with a 404 status and an error message for invalid comment keys', () => {
+    it('should respond with status 400 and an error message for invalid Review IDS', () => {
+      return request(app)
+      .post('/api/reviews/triangle/comments')
+      .send({username: 'bainesface', body: 'very nice stuff!'})
+      .expect(400)
+      .then(({body})=>{
+        expect(body.msg).toBe('Invalid ID')
+      })
+    });
+    it('should respond with a 404 status and an error message for an invalid username', () => {
       return request(app)
       .post('/api/reviews/3/comments')
       .send({username: 'sugar', body: 'very nice stuff!'})
@@ -180,7 +202,7 @@ describe("/api/reviews/review_id/comments", () => {
         expect(body.msg).toBe('Invalid username')
       })
     });
-    it('should respond with a 400 bad request for missing comment properties', () => {
+    it('should respond with a 400 bad request for missing required comment properties', () => {
       return request(app)
       .post('/api/reviews/3/comments')
       .send({username: 'bainesface'})
