422a2e0d859ffb1170ac54868476d37eb1348486
Faisal <faisalmaya@hotmail.com>
2023-04-17 16:49:24 +0100

    testing cors

--- db/app.js
+++ db/app.js
@@ -9,8 +9,8 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors())
 
 app.get('/api', getApiInstructions)
 app.get('/api/categories',getAllCategories)
