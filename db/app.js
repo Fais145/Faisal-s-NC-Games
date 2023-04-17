7ab179181f8e1de923bca58adcfbf062d0669721
Faisal <faisalmaya@hotmail.com>
2023-04-17 16:26:07 +0100

    .

--- db/app.js
+++ db/app.js
@@ -9,18 +9,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-const corsOrigin ={
-  origin:'http://localhost:3000',
-  credentials:true,            
-  optionSuccessStatus:200
-}
-app.use(cors(corsOrigin));
+app.use(cors());
 app.use(express.json());
 
 app.get('/api', getApiInstructions)
