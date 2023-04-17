9acc005fd18845f878893a87c8a0facaaa2f3311
Faisal <faisalmaya@hotmail.com>
2023-04-17 16:18:25 +0100

    .

--- db/app.js
+++ db/app.js
@@ -9,6 +9,12 @@ const cors = require('cors');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const corsOrigin ={
   origin:'http://localhost:3000',
   credentials:true,            
