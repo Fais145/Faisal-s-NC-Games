be3cfbc10e20127019cc1519f2c61ae2579f40a5
Faisal <faisalmaya@hotmail.com>
2023-06-01 18:19:07 +0100

    fixed old bug in my sorting query for comment count

--- db/Models/reviews.models.js
+++ db/Models/reviews.models.js
@@ -25,7 +25,7 @@ if(category){
     .query(
       `SELECT reviews.review_id, owner, title, designer, review_img_url, category, reviews.created_at, reviews.votes, COUNT(comments.comment_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id ` +dbQueryStr+
     ` GROUP BY reviews.review_id 
-    ORDER BY reviews.${sort} ${order};`,dbParamArr
+    ORDER BY ${sort} ${order};`,dbParamArr
     )
     .then(({ rows }) => {
       return rows;
